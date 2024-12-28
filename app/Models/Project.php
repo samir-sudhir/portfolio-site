3cd58504f50d7e2045d0e9d70686c180e71cb438
Samir Sudhir <samirsudhir30@gmail.com>
2024-12-28 15:36:51 +0500

    Updated Project Model and handled image url

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,13 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'description', 'image_path'];
+
+    // Append custom attribute to JSON response
+    protected $appends = ['full_image_url'];
+
+    // Accessor to generate the full URL
+    public function getFullImageUrlAttribute()
+    {
+        return asset('storage/' . $this->image_path);
+    }
 }
